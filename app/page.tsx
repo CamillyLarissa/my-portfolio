6f949c5777741bcff9c179bd2bd4da0b91ccc158
Camilly Larissa <camilly.costa@ufpi.edu.br>
2025-12-21 17:33:33 -0300

    feat: integrando  ShadcnUI e Tailwind CSS Closes #2

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,13 @@
-import Image from "next/image";
+
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 export default function Home() {
   return (
